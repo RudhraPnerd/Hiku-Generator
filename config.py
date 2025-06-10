9817241ca477905ff7318a6c9ea6d900b1a670dc
Rudhra <rudhra.thiru@gmail.com>
2025-06-10 18:05:20 +1000

    Hiku Generator v1 from Windows

---
+++ config.py
@@ -0,0 +1,8 @@
+CHARACTER_LIST = [">o", ":)", ":|", "^*^"]
+CHARACTER = "^*^" #If you want a custom kaomoji instead of using one of the presets, go into the list and add you custom
+                  #one. Then, in the CHARACTER variable, type the kaomoji within the double quotes. DO NOT ADD CUSTOM IF
+                  #THE CUSTOM ONE IS NOT IN THE LIST. THIS COULD LEAD TO SERIOUS ERRORS TO THE PROJECT
+SCREEN_TYPE_LIST = ["Console", "Window"]
+SCREEN_TYPE = "Console"
+HIKU_POEM_FILE = "assets/poems.json"
+
